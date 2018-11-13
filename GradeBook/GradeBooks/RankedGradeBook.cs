e0e218aa7269a9aa0840e1f0cd8809d7c8c1d323
pluralsighttester <->
2018-11-13 22:19:41 +0100

    Part2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +10,35 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            int studentsWithLowerGrades = Students.Count(s => s.AverageGrade < averageGrade);
+
+            double percentageStudentsWithLowerGrades = (double)studentsWithLowerGrades / Students.Count * 100;
+            if (percentageStudentsWithLowerGrades >= 80)
+            {
+                return 'A';
+            }
+            else if(percentageStudentsWithLowerGrades >= 60)
+            {
+                return 'B';
+            }
+            else if (percentageStudentsWithLowerGrades >= 40)
+            {
+                return 'C';
+            }
+            else if (percentageStudentsWithLowerGrades >= 20)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
